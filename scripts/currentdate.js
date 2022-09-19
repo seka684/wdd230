a11f397c345f75c5b224cdd0a388cdc9a69092cd
seka684 <seg21001@byui.edu>
2022-09-19 09:29:05 -1100

    updated last modified

--- scripts/currentdate.js
+++ scripts/currentdate.js
@@ -1,13 +1,4 @@
-const currentdate = new Date();
-const options = { day: "numeric", month: "numeric", year: "numeric" };
-document.getElementById("currentdate").textContent = new Date().toLocaleString(
-  "en-US",
-  options
-);
-
 document.getElementById("year").textContent = new Date().getFullYear();
 
-const today = new Date();
-const currenttime =
-  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-document.getElementById("currenttime").textContent = currenttime;
+const lastmod = new Date(document.lastModified);
+document.getElementById('lastmodified').innerHTML = lastmod;
