bbafa6a8c99b6dcca0f8f7d82da873c37791b311
seka684 <seg21001@byui.edu>
2022-10-14 13:17:03 -1100

    bom

--- practice/scripts/chapter.js
+++ practice/scripts/chapter.js
@@ -1,25 +1,24 @@
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const list = document.querySelector('list');
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
 
-button.addEventListener('click', () => {
-    const mychapter = input.value;
-    input.value = '';
+button.addEventListener("click", () => {
+  const myItem = input.value;
+  input.value = "";
 
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    const listBtn = document.createElement('button');  
+  const listItem = document.createElement("li");
+  const listText = document.createElement("span");
+  const listBtn = document.createElement("button");
 
-    
-    listItem.appendChild(listText);
-    listText.textContent = mychapter;
-    listItem.appendChild(listBtn);
-    listBtn.textContent = 'Delete';
-    list.appendChild(listItem);
+  listItem.appendChild(listText);
+  listText.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = "Delete";
+  list.appendChild(listItem);
 
-    listBtn.addEventListener('click', () => {
-      list.removeChild(listItem);
-    });
+  listBtn.addEventListener("click", () => {
+    list.removeChild(listItem);
+  });
 
-    input.focus();
+  input.focus();
 });
