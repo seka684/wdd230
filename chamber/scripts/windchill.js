225d09f2f78003aca351974311ec4b8d84d4e121
seka684 <seg21001@byui.edu>
2022-11-01 17:47:12 -1100

    gznzgd

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,5 @@
+const t = document.getElementsByClassName(".currentTemp");
+const sp = document.getElementsByClassName(".windSpeed");
+const wc = "N/A";
+
+document.getElementById("#windChill").textContent = wc;
